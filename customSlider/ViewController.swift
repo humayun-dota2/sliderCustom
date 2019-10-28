55f2fea888cbeaf813604718c5d441d672417cd0
humayun kabir <humayun_kabir@appnap.io>
2019-10-28 10:04:00 +0600

    Initial Commit

---
+++ customSlider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  customSlider
+//
+//  Created by Appnap WS02 on 28/10/19.
+//  Copyright © 2019 Appnap WS02. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
